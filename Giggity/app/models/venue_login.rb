fa94588e9fe0eaa310ff4b57b7fa415b04726608
Imade Osifo <toxsik88@gmail.com>
2019-12-04 13:42:18 -0500

    i made some changes

--- Giggity/app/models/venue_login.rb
+++
@@ -1,3 +0,0 @@
-class VenueLogin < ActiveRecord::Base
-    has_secure_password
-end 
--- Giggity/config/routes.rb
+++ Giggity/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root :to => 'home#user_type', :as => 'main_menu' 
   get '/venue_user', :to => 'venue_user#show', :as => 'venue_user' 
   get '/artist_user', :to => 'artist_user#show', :as => 'artist_user' 
+  get 'venue_login', to: 'sessions#new'
+  post 'venue_login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  
   resources :agency_artist_songs
   resources :local_artist_songs
   resources :agency_artists
